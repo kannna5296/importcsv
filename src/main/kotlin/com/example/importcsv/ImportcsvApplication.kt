250658f2ac062cd047dc834044a4b7d301cd24cb
kannna5296 <kannnahacks5296@gmail.com>
2022-07-03 13:44:06 +0900

    first

---
+++ src/main/kotlin/com/example/importcsv/ImportcsvApplication.kt
@@ -0,0 +1,11 @@
+package com.example.importcsv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImportcsvApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImportcsvApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/importcsv/ImportcsvApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.importcsv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImportcsvApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

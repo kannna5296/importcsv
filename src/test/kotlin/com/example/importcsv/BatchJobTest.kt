455e00e3c4f24a9c0875125cc63da33090caea7b
kannna5296 <kannnahacks5296@gmail.com>
2022-07-10 14:58:20 +0900

    諸々修正

---
+++ src/test/kotlin/com/example/importcsv/BatchJobTest.kt
@@ -0,0 +1,21 @@
+package com.example.importcsv
+
+import com.example.importcsv.job.BatchJob
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.batch.test.context.SpringBatchTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringRunner
+
+@SpringBatchTest
+@RunWith(SpringRunner::class)
+@ContextConfiguration(classes = [BatchJob::class])
+class BatchJobTest {
+
+    @Test
+    fun `正常系1件`() {
+        val input = "test"
+        assertEquals("test",input)
+    }
+}
